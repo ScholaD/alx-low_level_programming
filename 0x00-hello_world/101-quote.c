daec85367f961390d9f31af4444ee311d634540e
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-16 11:53:23 -0700

    two

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - this is the main function
+* Return: always 1
+*
+*/
+int main(void)
+{
+fput("\"Programming is like building a multilingual puzzle");
+return (1);
+}

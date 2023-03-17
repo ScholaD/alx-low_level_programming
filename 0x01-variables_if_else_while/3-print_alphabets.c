ad7505e6a9ea5f69aa0ad92412fc58ed3a642214
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-17 07:56:45 -0700

    “three”

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+ /* Print lowercase alphabet */
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+ /* Print uppercase alphabet */
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');  /* Print newline character */
+	return (0);
+}
+

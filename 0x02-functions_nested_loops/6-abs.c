099cbc8eec8aff3d61275dac146fe64441aa2878
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-22 07:34:33 -0700

    sixth

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *@num:character in ASCII
+ * Return: Return num for greater than -1 for less  than
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
+

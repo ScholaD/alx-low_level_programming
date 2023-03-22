4b843704d66a66df2328951c94c2086e4d46b25d
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-22 06:47:31 -0700

    number four

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *@n:character in ASCII
+ * Return: Return 1 for positive -1 for lesser than  0 for other
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		return (0);
+	}
+}

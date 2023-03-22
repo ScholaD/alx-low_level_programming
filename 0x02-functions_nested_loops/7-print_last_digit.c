835ed153c1520ce9578b83564af3ccac77d00377
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-22 07:59:37 -0700

    man is working

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *@num:character in ASCII
+ * Return: Retur last_digit
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	_putchar (last_ digit + '0');
+	return (last_digit);
+}

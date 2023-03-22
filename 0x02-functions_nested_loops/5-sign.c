aa20c60be9a73cddf530a139ab3537e05245363d
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-22 07:13:26 -0700

    Fourth

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_sign - Entry point
  *@n:character in ASCII
@@ -9,17 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
+	_putchar('\n')
 }

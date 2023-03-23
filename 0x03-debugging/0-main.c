1fe239d6728993b903419c0d5855f5be19e3f8c2
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-23 01:22:18 -0700

    question one

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *positive_or_negative - tests function that prints
+ *@i: value of i
+ *Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}

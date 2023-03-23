b587e06f8a923dcbcc86621cafd58efaa6db0f46
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-23 01:56:08 -0700

    second attempt

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,11 @@
  *@i: value of i
  *Return: 0
  */
-void positive_or_negative(int i)
+int main (void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *positive_or_negative - Entry point
+ *@i- the integer
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}  /* your code goes there */
+	return (0);
+}

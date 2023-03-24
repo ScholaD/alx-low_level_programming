53aaec2fa0e8c6604c8e259d39915cf7324b197f
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 02:43:44 -0700

    hello world

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,15 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	int i;
-
-	for (i = 0; i < n; i++)
+	else
 	{
-		_putchar('_');
+		int i;
+
+		for (i = 1; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

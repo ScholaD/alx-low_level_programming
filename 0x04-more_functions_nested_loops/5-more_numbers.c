0c7f1bd9f61de61b56089acfb2596ebaf5313129
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 02:13:30 -0700

    its good

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,21 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i, j, num;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		for (int j = 0; j <= 14; j++)
 		{
+			num = j;
+
 			if (j >= 10)
 			{
 				_putchar('1');
+				num = j % 10;
 			}
-			_putchar('0' + (j % 10));
+			_putchar(num + '0');
 		}
 		_putchar('\n');
-		i++;
 	}
 }

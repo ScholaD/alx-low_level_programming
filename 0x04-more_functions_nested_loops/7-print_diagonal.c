9d70b90de0cdac76a8e1571e4c4a5c9cfbe33f39
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 06:30:21 -0700

    hardworking

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,6 @@
   */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
 		int i, j;
 
 		for (i = 0; i < n; i++)
@@ -24,5 +18,4 @@ void print_diagonal(int n)
 			}
 		}
 		_putchar('\n');
-	}
 }

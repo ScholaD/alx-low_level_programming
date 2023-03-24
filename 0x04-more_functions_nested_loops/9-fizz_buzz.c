0764209005c9e9e63db8425f539c8c9c8d64b186
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 07:45:44 -0700

    “hello”

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
@@ -10,24 +10,27 @@
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s ", fb);
 		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s ", f);
 		}
 		else if (i % 5 == 0  && i % 3 != 0)
 		{
-			printf("Buzz ");
+			printf("%s ", b);
 		}
-		else if (i == 1)
+		else if (i == 100)
 		{
-			printf("%d ", i);
+			printf("%s", b);
 		}
 		else
 		{

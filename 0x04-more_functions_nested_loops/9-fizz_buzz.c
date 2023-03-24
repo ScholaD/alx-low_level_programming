7d3899bb6e8fd05e42cbf241933b77940839b12c
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 07:58:02 -0700

    alx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
@@ -8,7 +9,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)

0b3288054bbf51234bd8a24d378f335698d1a091
ScholaD <ifeomaegbeonu@gmail.com>
2023-03-24 01:37:31 -0700

    its well

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - The main point
+ *@a: for a input
+ *@b: for b input
+ *Return: returns result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}

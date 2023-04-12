eeb0e1b464eb6c6f8c50885ae39f31f270768802
ScholaD <ifeomaegbeonu@gmail.com>
2023-04-12 12:21:33 -0700

    hello

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	char *result = malloc(len1 + len2 + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (size_t j = 0; j < len2; j++)
+	{
+		result[i++] = s2[j];
+	}
+	result[i] = '\0';
+	return (result);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **met;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	met = malloc(sizeof(int *) * height);
+	if (met == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		met[x] = malloc(sizeof(int) * width);
+		if (met[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(met[x]);
+			free(met);
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			met[x][y] = 0;
+	}
+	return (met);
+}

fac4235d96eabf4fb7c6e5ea75bb9e1b98aaa077
ScholaD <ifeomaegbeonu@gmail.com>
2023-06-21 11:01:27 -0700

    im happy that im understanding the task and doing it

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-	for (iterate = 0; i < size; iterate++)
+	for (iterate = 0; iterate < size; iterate++)
 	{
 		action(array[iterate]);
 	}

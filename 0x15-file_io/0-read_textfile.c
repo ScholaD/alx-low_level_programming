3f1a784b6c32353f7be67c640734d9f3e6678cb5
ScholaD <ifeomaegbeonu@gmail.com>
2023-08-07 12:38:39 -0700

    Im still confused buut i know can do it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t w;
+	ssize_t wt;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
@@ -19,8 +19,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	wt = write(STDOUT_FILENO, buf, t);
 	free(buf);
 	close(fd);
-	return (w);
+	return (wt);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  **/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, wt, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,8 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-	if (o == -1 || w == -1)
+	wt = write(o, text_content, len);
+	if (o == -1 || wt == -1)
 		return (-1);
 	close(o);
 	return (1);

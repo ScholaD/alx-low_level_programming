b0b304fe3b5539152c7bcfbe0f7c7e36dba3f4a4
ScholaD <ifeomaegbeonu@gmail.com>
2023-09-08 20:24:57 -0700

    We did c today

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,9 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t))
-		if (ht == NULL)
-			return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
